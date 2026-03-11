54688c17a5f3d7d12ab112968e57668c2f85ca7a
xy.art70 <xy.art70@gmail.com>
2026-03-11 08:59:49 +0000

    chore: @vite-pwa/create-pwa

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'Plain2TeX',
+      short_name: 'EQ2TeX',
+      description: 'Convert plain-text math expressions to professionally typeset LaTeX instantly. Export as PNG or SVG with one click.',
+      theme_color: '#0e69ff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
