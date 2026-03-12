b8e5b5e1b10525a5715ed890da82a42d9b49e5f8
Art 70x <206083185+art70x@users.noreply.github.com>
2026-03-12 05:09:08 +0000

    feat(analytics): add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import App from '@/app.tsx'
 import '@/main.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.querySelector('#root')!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>,
 )
